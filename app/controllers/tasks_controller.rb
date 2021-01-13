db1d007e2e3bd91f1b2a0b744eaaf5a40ba3cbb0
Ubuntu <ubuntu@ip-172-31-43-160.us-east-2.compute.internal>
2021-01-13 13:24:24 +0000

    second

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+        @task = Task.all
        
     end
     
     def new
-        
+        @task = Task.new
     end
     
     def check
@@ -13,16 +13,23 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find_by(params[:id])
+        @task = Task.find_by(id: params[:id])
+        @allday = Task.find_by(params[:allday])
     end
     
     def update
         @task = Task.find_by(params[:id])
-        if @task.update(title: params[:title], start: params[:start], finish: params[:finish], allday: params[:allday], memo: params[:memo])
+        if @task.update(params.require(:task).permit(:title, :start, :finish, :allday, :memo))
+            flash[:notice] = "スケジュールの情報を更新しました"
             redirect_to "/tasks"
         else
-            render "/tasks/edit"
+            render "edit"
         end
+        # if @task.update(title: params[:title], start: params[:start], finish: params[:finish], allday: params[:allday], memo: params[:memo])
+        #     redirect_to "/tasks"
+        # else
+        #     render "/tasks/edit"
+        # end
     end
     
 
@@ -31,9 +38,13 @@ class TasksController < ApplicationController
     end
     
     def create
-        Task.create(title: params[:title], start: params[:start], finish: params[:finish], allday: params[:allday], memo: params[:memo])
-        
-        redirect_to "/tasks"
+        @task = Task.new(params.require(:task).permit(:title, :start, :finish, :allday, :memo))
+        if @task.save
+            flash[:notice] = "スケジュールを作成しました"
+            redirect_to "/tasks"
+        else
+            render "tasks/new"
+        end
     end
     
     def destroy
